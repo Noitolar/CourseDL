c8d91a41f440516316662e4144eb9ac37a310d79
马天行 <matianxing2000@163.com>
2023-06-01 00:19:30 +0800

    添加了CIFAR数据集的处理。

--- utils/cv/dataset.py
+++ utils/cv/dataset.py
@@ -13,9 +13,37 @@ def get_dataset(config: ucv.config.ConfigObject):
         val_set = vdatasets.MNIST(root=f"./datasets/mnist", train=False, transform=config.val_preprocess, download=True)
         return trn_set, val_set
     elif config.dataset == "cifar10":
-        return None, None
+        if config.trn_preprocess is None:
+            config.trn_preprocess = trans.Compose([
+                trans.Resize((224, 224)),
+                trans.RandomHorizontalFlip(p=0.5),
+                trans.ToTensor(),
+                trans.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+        if config.val_preprocess is None:
+            config.val_preprocess = trans.Compose([
+                trans.ToTensor(), 
+                trans.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+        trn_set = vdatasets.CIFAR10(root=f"./datasets/cifar10", train=True, transform=config.trn_preprocess, download=True)
+        val_set = vdatasets.CIFAR10(root=f"./datasets/cifar10", train=False, transform=config.val_preprocess, download=True)
+        return trn_set, val_set
     elif config.dataset == "cifar100":
-        return None, None
+        if config.trn_preprocess is None:
+            config.trn_preprocess = trans.Compose([
+                trans.Resize((224, 224)),
+                trans.RandomHorizontalFlip(p=0.5),
+                trans.ToTensor(),
+                trans.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+        if config.val_preprocess is None:
+            config.val_preprocess = trans.Compose([
+                trans.ToTensor(), 
+                trans.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            ])
+        trn_set = vdatasets.CIFAR100(root=f"./datasets/cifar100", train=True, transform=config.trn_preprocess, download=True)
+        val_set = vdatasets.CIFAR100(root=f"./datasets/cifar100", train=False, transform=config.val_preprocess, download=True)
+        return trn_set, val_set
     else:
         assert config.trn_preprocess is not None
         assert config.val_preprocess is not None
